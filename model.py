d262ac14b10560ade35fc25c624e4bd845e6e3f8
nmautz <nmautz@zagmail.gonzaga.edu>
2023-09-27 09:46:34 -0700

    all posible moves

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import heapq
 
 def nim_sum(numbers):
   total = 0
@@ -21,3 +22,40 @@ class Node:
     self.h = calculate_heuristic_score(state)
     self.f = self.g + self.h
 
+  def __lt__(self, other):
+    return self.f < other.f
+  
+  def __gt__(self, other):
+    return self.f > other.f
+  
+
+class AStar:
+
+  def __init__(self, start_state):
+    self.nodes_to_visit = [Node(start_state, None)]
+    heapq.heapify(self.nodes_to_visit)
+    
+  def expand_node(self, node):
+    #add all possible next states to self.tovisit
+    for number in node.state:
+      if number > 0:
+        for i in range(1, number+1):
+          new_state = node.state.copy()
+          new_state[node.state.index(number)] -= i
+          heapq.heappush(self.nodes_to_visit, Node(new_state, node))
+
+  
+
+
+  def run(self):
+    while self.nodes_to_visit:
+      node = heapq.heappop(self.nodes_to_visit)
+      if nim_sum(node.state) == 0:
+        return node
+      self.expand_node(node)
+
+state = [1,2,3]
+model = AStar(state)
+model.expand_node(Node(state, None))
+for node in model.nodes_to_visit:
+  print(node.state)
