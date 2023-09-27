b325549198ab62b3086c0f60401c09c49f411dc6
nmautz <nmautz@zagmail.gonzaga.edu>
2023-09-27 09:21:40 -0700

    node structure

--- model.py
+++ model.py
@@ -5,3 +5,19 @@ def nim_sum(numbers):
     total ^= number
   return total
 
+
+def calculate_heuristic_score(state):
+  return nim_sum(state)
+
+class Node:
+  
+  def __init__(self, state, parent_node):
+    self.state = state
+    self.parent_node = parent_node
+    if parent_node != None:
+      self.g = parent_node.g +1
+    else: 
+      self.g = 0
+    self.h = calculate_heuristic_score(state)
+    self.f = self.g + self.h
+
