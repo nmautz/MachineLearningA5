81567e4830934aa003cfe300247651e7255edf6a
nmautz <nmautz@zagmail.gonzaga.edu>
2023-09-28 16:26:31 -0700

    refactored as nim player class

--- model.py
+++ model.py
@@ -1,22 +1,36 @@
 
-def nim_sum(state_arr):
-  total = 0
-  for number in state_arr:
-    total ^= number
-  return total
 
 
-def next_states(state_arr):
 
-  next_states_arr = []  
-  for number in state_arr:
-    if number > 0:
-      for i in range(1, number+1):
-        new_state = state_arr.copy()
-        new_state[state_arr.index(number)] -= i
-        next_states_arr.append(new_state)
-  return next_states_arr
 
-def is_game_won(state_arr):
-  return len(next_states(state_arr)) == 0
+
+
+
+
+class NimPlayer:
   
+  def __init__(self):
+    pass
+
+  def is_game_won(self, state_arr):
+    return len(self.get_next_states(state_arr)) == 0
+    
+  def nim_sum(self, state_arr):
+    total = 0
+    for number in state_arr:
+      total ^= number
+    return total
+
+
+  def get_next_states(self, state_arr):
+    next_states_arr = []  
+    for number in state_arr:
+      if number > 0:
+        for i in range(1, number+1):
+          new_state = state_arr.copy()
+          new_state[state_arr.index(number)] -= i
+          next_states_arr.append(new_state)
+    return next_states_arr
+
+  def play(self, state_arr):
+    return self.get_next_states(state_arr)[0]
