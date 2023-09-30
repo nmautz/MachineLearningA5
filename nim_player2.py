f987184c82b086e057217c7204b14496cac98ca2
nmautz <nmautz@zagmail.gonzaga.edu>
2023-09-30 15:11:48 -0700

    fixed compete bug

--- nim_player2.py
+++ nim_player2.py
@@ -28,30 +28,11 @@ class NimPlayer:
             next_states_arr.append(new_state.copy())
     return next_states_arr
 
-  def get_state_score(self,state_arr, is_friendly_turn):
-    if state_arr == [0,0,0,0] and is_friendly_turn:
-      return -1
-    elif state_arr == [0,0,0,0] and not is_friendly_turn:
-      return 1
-    else:
-      next_states = self.get_next_states(state_arr)
-      score = 0
-      for next_state in next_states:
-        score += self.get_state_score(next_state, not is_friendly_turn)
-      return score
-
   def play(self, state_arr):
     next_states = self.get_next_states(state_arr)
     if len(next_states) == 0:
-      print(state_arr)
-      return state_arr
-    best_state = next_states[0]
-    best_score = self.get_state_score(best_state, False)
-    for next_state in next_states:
-      score = self.get_state_score(next_state, False)
-      if score > best_score:
-        best_state = next_state
-        best_score = score
-    return best_state
+      return [0,0,0,0]
+  
+
       
-    
+    return next_states[0]
