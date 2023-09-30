79a5c86778724de7c2f806c1402343123f0a030b
nmautz <nmautz@zagmail.gonzaga.edu>
2023-09-30 12:24:03 -0700

    player oplay

--- nim_player_vs_ai.py
+++ nim_player_vs_ai.py
@@ -4,6 +4,7 @@ import traceback
 import time
 import sys
 import numpy as np
+import random
 
 
 
@@ -49,3 +50,58 @@ except:
     board = [1,3,5,7]
     print_help()
     print("Using default board: ", board)
+
+
+def is_board_lost(board):
+    for i in range(len(board)):
+        if board[i] > 0:
+            return False
+    return True
+
+winner = None
+player_first_move = random.randint(0,1)
+if player_first_move == 0:
+    print("Player goes first")
+else:
+    print("AI goes first")
+
+def is_valid_move(board, new_board):
+  columns_changed = 0
+  
+  for i in range(len(board)):
+      if board[i] != new_board[i]:
+          columns_changed += 1
+          if board[i] < new_board[i]:
+              return False
+          if board[i] < 0:
+              return False
+  return columns_changed == 1    
+
+def player_play(board):
+
+    stupid_bool = False
+    new_board = [-1,-1,-1,-1]
+    print(board)
+    print("---------")
+    while not is_valid_move(board, new_board):
+      if stupid_bool:
+        print("Invalid move, try again")
+      else:
+        stupid_bool = True
+
+      print("Enter new board state")
+      print("---------")
+      new_board_str = input()
+      new_board = [int(x) for x in new_board_str.split(",")]
+
+
+while winner == None:
+    print(board)
+    if player_first_move == 0:
+        print("Player's turn")
+        #TODO
+    else:
+        print("AI's turn")
+        board = np.play(board)
+        print(board)
+        
