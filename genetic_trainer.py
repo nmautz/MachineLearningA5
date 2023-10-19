ee5531c6deacc5b4336199a93fdedac3017765a5
nmautz <nmautz@zagmail.gonzaga.edu>
2023-10-19 15:42:08 -0700

    implementing functions

--- genetic_trainer.py
+++ genetic_trainer.py
@@ -26,11 +26,35 @@ migration_rate = 0.15
 reset_bottom_rate = 0.1
 
 
+def get_next_states(self, state_arr):
+    next_states_arr = []  
+    for number in state_arr:
+        if number > 0:
+            for i in range(1, number+1):
+                new_state = state_arr.copy()
+                new_state[state_arr.index(number)] -= i
+                if not np.array_equal(new_state, state_arr):
+                    if not new_state in next_states_arr:
+                        next_states_arr.append(new_state.copy())
+    return next_states_arr
+
 def crossover(parent1, parent2):
-    pass
+    parent1_dna = parent1.dna
+    parent2_dna = parent2.dna
+    combined_dna = parent1_dna + parent2_dna
+    for gene in combined_dna:
+        for gene2 in combined_dna:
+            if gene.prev_board == gene2.prev_board and gene.curr_board == gene2.curr_board:
+                combined_dna.remove(gene2)
+    child = genetic_nim_player.GeneticNimPlayer()
+    child.dna = combined_dna
 
 def mutate(individual):
-    pass
+    for gene in individual.dna:
+        if random.random() < mutation_rate:
+            next_boards = get_next_states(gene.curr_board)
+            next_boards = random.shuffle(next_boards)
+            gene.next_board = next_boards[0]
 
 def get_next_generation(current_population):
     pass
